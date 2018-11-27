f451cd3616658936a5f3a1c0fd77ab38c90b780b
Joseph Huang <g6.joseph.huang@gmail.com>
2018-11-27 13:52:34 -0500

    updated accel file

--- Main_Acc/Accelerometer_Reader/Accelerometer_Reader.ino
+++ Main_Acc/Accelerometer_Reader/Accelerometer_Reader.ino
@@ -1,36 +1,92 @@
-// I2C Scanner
-// Written by Nick Gammon
-// Date: 20th April 2011
+#include<Wire.h>
 
-#include <Wire.h>
+#define XYZ_DATA 0x32
+#define POWER_CTL 0x2D
+#define ACCE_ADDR 0x53
+#define FIFO_CTL 0x38
+#define DATA_FORMAT 0x31
+#define THRESH_ACT 0x24
+#define INT_ENABLE 0x2E
+#define INT_MAP 0x2F
+#define ACT_INACT_CTL 0x27
+
+byte values[6];
+char output[512];
 
 void setup() {
- Serial.begin (9600);
- Serial.println ();
- Serial.println ("I2C scanner. Scanning ...");
- byte count = 0;
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(100);
+  Wire.begin();
 
- Wire.begin();
+  //Set to measurement mode
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(POWER_CTL);
+  Wire.write(8);
+  Wire.endTransmission();
 
- for (byte i = 1; i < 120; i++)
- {
-   Serial.println (i, DEC);
-   Wire.beginTransmission (i);
-   if (Wire.endTransmission () == 0)
-     {
-     Serial.print ("Found address: ");
-     Serial.print (i, DEC);
-     Serial.print (" (0x");
-     Serial.print (i, HEX);
-     Serial.println (")");
-     count++;
-     } // end of good response
-    delay (5);  // give devices time to recover
- } // end of for loop
- Serial.println ("Done.");
- Serial.print ("Found ");
- Serial.print (count, DEC);
- Serial.println (" device(s).");
-}  // end of setup
+  //Set to stream mode
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(FIFO_CTL);
+  Wire.write(0b10011111);
+  Wire.endTransmission();  
 
-void loop() {}
+  //Set to 4g full resolution
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(DATA_FORMAT);
+  Wire.write(0b00001011);
+  Wire.endTransmission();
+
+  //Set threshold for actibity interrupt
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(THRESH_ACT);
+  Wire.write((byte)(2.0/(0.015625))); //2 g
+  Wire.endTransmission();
+
+  //choose y, z axis for measurement against threshold
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(ACT_INACT_CTL);
+  Wire.write(0b00110000);
+  Wire.endTransmission();
+
+  //send all data to interrupt 1
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(INT_MAP);
+  Wire.write(0b00000000);
+  Wire.endTransmission();
+
+  //Enable activity interrupt
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(INT_ENABLE);
+  Wire.write(0b00010000);
+  Wire.endTransmission();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x, y, z;
+  
+  Wire.beginTransmission(ACCE_ADDR);
+  Wire.write(XYZ_DATA);
+  Wire.endTransmission();
+
+  Wire.requestFrom(ACCE_ADDR, 6);
+
+  int i = 0;
+  while(Wire.available())    // slave may send less than requested
+  { 
+    values[i] = Wire.read();
+    i++;
+  } 
+
+  x = (((int)values[1]) << 8) | values[0];
+  y = (((int)values[3]) << 8) | values[2];
+  z = (((int)values[5]) << 8) | values[4];
+  
+  sprintf(output, "%d %d %d", x, y, z);
+  Serial.print(output);
+  
+  Serial.println();
+  delay(500);
+}
+
