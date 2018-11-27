17ce1ffbaee08cb3821cae0c10f8218c8817c156
Joseph Huang <g6.joseph.huang@gmail.com>
2018-11-27 13:40:41 -0500

    Added Acc Code

---
+++ Main_Acc/Accelerometer_Reader/Accelerometer_Reader.ino
@@ -0,0 +1,36 @@
+// I2C Scanner
+// Written by Nick Gammon
+// Date: 20th April 2011
+
+#include <Wire.h>
+
+void setup() {
+ Serial.begin (9600);
+ Serial.println ();
+ Serial.println ("I2C scanner. Scanning ...");
+ byte count = 0;
+
+ Wire.begin();
+
+ for (byte i = 1; i < 120; i++)
+ {
+   Serial.println (i, DEC);
+   Wire.beginTransmission (i);
+   if (Wire.endTransmission () == 0)
+     {
+     Serial.print ("Found address: ");
+     Serial.print (i, DEC);
+     Serial.print (" (0x");
+     Serial.print (i, HEX);
+     Serial.println (")");
+     count++;
+     } // end of good response
+    delay (5);  // give devices time to recover
+ } // end of for loop
+ Serial.println ("Done.");
+ Serial.print ("Found ");
+ Serial.print (count, DEC);
+ Serial.println (" device(s).");
+}  // end of setup
+
+void loop() {}
